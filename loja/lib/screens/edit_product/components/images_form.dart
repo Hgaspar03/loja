ff1166a22ebb898fe8173a334ab6eba8d38883e4
Helenio Gaspar <heleniogaspar@gmail.com>
2021-03-19 18:15:31 +0200

    ediat produtos

--- loja/lib/screens/edit_product/components/images_form.dart
+++ loja/lib/screens/edit_product/components/images_form.dart
@@ -13,6 +13,12 @@ class ImageForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FormField<List<dynamic>>(
+      validator: (images) {
+        if (images != null && images.isEmpty) {
+          return "Insira pelo menos uma imagem";
+        }
+        return null;
+      },
       initialValue: List.from(product.images),
       builder: (state) {
         void onImageSelected(File file) {
@@ -21,70 +27,83 @@ class ImageForm extends StatelessWidget {
           Navigator.of(context).pop();
         }
 
-        return AspectRatio(
-          aspectRatio: 1,
-          child: Carousel(
-            images: state.value.map((image) {
-              return Stack(
-                fit: StackFit.expand,
-                children: [
-                  if (image is String)
-                    Image.network(
-                      image,
-                      fit: BoxFit.cover,
-                    )
-                  else
-                    Image.file(image as File, fit: BoxFit.cover),
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: IconButton(
-                        onPressed: () {
-                          state.value.remove(image);
-                          state.didChange(state.value);
-                        },
-                        icon: Icon(Icons.remove)),
-                  )
-                ],
-              );
-            }).toList()
-              ..add(
-                Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Material(
-                      color: Colors.grey[100],
-                      child: IconButton(
-                        iconSize: 54,
-                        color: Theme.of(context).primaryColor,
-                        icon: Icon(Icons.add_a_photo),
-                        onPressed: () {
-                          if (Platform.isAndroid)
-                            showModalBottomSheet(
-                              context: context,
-                              builder: (_) => ImageSourceSheet(
-                                onImageSelect: onImageSelected,
-                              ),
-                            );
-                          else {
-                            showCupertinoModalPopup(
-                              context: context,
-                              builder: (_) => ImageSourceSheet(
-                                onImageSelect: onImageSelected,
-                              ),
-                            );
-                          }
-                        },
-                      ),
+        return Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Carousel(
+                images: state.value.map((image) {
+                  return Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      if (image is String)
+                        Image.network(
+                          image,
+                          fit: BoxFit.cover,
+                        )
+                      else
+                        Image.file(image as File, fit: BoxFit.cover),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                            onPressed: () {
+                              state.value.remove(image);
+                              state.didChange(state.value);
+                            },
+                            icon: Icon(Icons.remove)),
+                      )
+                    ],
+                  );
+                }).toList()
+                  ..add(
+                    Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        Material(
+                          color: Colors.grey[100],
+                          child: IconButton(
+                            iconSize: 54,
+                            color: Theme.of(context).primaryColor,
+                            icon: Icon(Icons.add_a_photo),
+                            onPressed: () {
+                              if (Platform.isAndroid)
+                                showModalBottomSheet(
+                                  context: context,
+                                  builder: (_) => ImageSourceSheet(
+                                    onImageSelect: onImageSelected,
+                                  ),
+                                );
+                              else {
+                                showCupertinoModalPopup(
+                                  context: context,
+                                  builder: (_) => ImageSourceSheet(
+                                    onImageSelect: onImageSelected,
+                                  ),
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                dotSize: 6,
+                dotColor: Theme.of(context).primaryColor,
+                dotBgColor: Colors.transparent,
+                autoplay: false,
+                dotSpacing: 15,
               ),
-            dotSize: 6,
-            dotColor: Theme.of(context).primaryColor,
-            dotBgColor: Colors.transparent,
-            autoplay: false,
-            dotSpacing: 15,
-          ),
+            ),
+            if (state.hasError)
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.only(top: 16, left: 16),
+                child: Text(
+                  state.errorText,
+                  style: TextStyle(color: Colors.red, fontSize: 12),
+                ),
+              )
+          ],
         );
       },
     );
--- loja/lib/screens/edit_product/edit_product_screen.dart
+++ loja/lib/screens/edit_product/edit_product_screen.dart
@@ -3,9 +3,11 @@ import 'package:loja/models/products.dart';
 import 'package:loja/screens/edit_product/components/images_form.dart';
 
 class EditProductScreen extends StatelessWidget {
-  const EditProductScreen(this.product);
+  EditProductScreen(this.product);
 
   final Product product;
+
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +16,71 @@ class EditProductScreen extends StatelessWidget {
         title: const Text("Editar Anuncio"),
         centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          ImageForm(product),
-        ],
+      backgroundColor: Colors.white,
+      body: Form(
+        key: formkey,
+        child: ListView(
+          children: [
+            ImageForm(product),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextFormField(
+                    initialValue: product.name,
+                    decoration: InputDecoration(
+                      hintText: "Titulo",
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                    validator: (name) {
+                      if (name.length < 6) {
+                        return "Titulo muito curto";
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4),
+                    child: Text(
+                      "A partir de",
+                      style: TextStyle(color: Colors.grey[600], fontSize: 13),
+                    ),
+                  ),
+                  Text(
+                    "... Mt",
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16, bottom: 8),
+                    child: Text(
+                      "Descricao",
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(),
+                    onPressed: () {
+                      if (formkey.currentState.validate()) {
+                        print("Valido");
+                      } else {
+                        print("Invalido");
+                      }
+                    },
+                    child: const Text("Salvar"),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
